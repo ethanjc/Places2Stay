03bd67d957c7e88ef64d57717c61ce15672c9911
Ethan Coward <ethanjcoward@gmail.com>
2022-05-18 00:17:58 -0700

    add fonts

--- src/components/base/text/Text.tsx
+++ src/components/base/text/Text.tsx
@@ -1,20 +1,22 @@
-import * as React from 'react';
-import {Text as StockText, StyleSheet} from 'react-native';
+import * as React from 'react'
+import { Text as StockText, StyleSheet } from 'react-native'
 
-import {Props} from './Text.types';
+import { Props } from './Text.types'
 
-const Text = ({children, style, variant = 'body', color = '#000'}: Props) => (
-  <StockText style={[styles[variant], {color}, style]}>{children}</StockText>
-);
+const Text = ({ children, style, variant = 'body', color = '#000' }: Props) => (
+  <StockText style={[styles[variant], { color }, style]}>{children}</StockText>
+)
 
 const styles = StyleSheet.create({
   body: {
     fontSize: 12,
     lineHeight: 15,
+    fontFamily: 'Encode',
   },
   heading: {
     fontSize: 24,
+    fontFamily: 'encode',
   },
-});
+})
 
-export default Text;
+export default Text
